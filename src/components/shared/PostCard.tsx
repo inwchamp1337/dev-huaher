8542372ada62f91dd83511f1cc6d92abe441b6ca
TxBlnwza <154392815+TxBlnwza@users.noreply.github.com>
2024-02-27 21:09:26 +0700

    แก้สีหน้าหลัก

--- src/components/shared/PostCard.tsx
+++ src/components/shared/PostCard.tsx
@@ -17,7 +17,7 @@ const PostCard = ({ post }: PostCardProps) => {
   return (
     <div className="post-card">
       <div className="flex-between">
-        <div className="flex items-center gap-3">
+        <div className="flex items-center gap-3 ">
           <Link to={`/profile/${post.creator.$id}`}>
             <img
               src={

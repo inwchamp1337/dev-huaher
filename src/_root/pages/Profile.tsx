13deb5532c1b43463ed36f4244064317729a1da9
TxBlnwza <154392815+TxBlnwza@users.noreply.github.com>
2024-03-03 22:28:30 +0700

    profile ดำ

--- src/_root/pages/Profile.tsx
+++ src/_root/pages/Profile.tsx
@@ -21,7 +21,7 @@ interface StabBlockProps {
 const StatBlock = ({ value, label }: StabBlockProps) => (
   <div className="flex-center gap-2">
     <p className="small-semibold lg:body-bold text-primary-500">{value}</p>
-    <p className="small-medium lg:base-medium text-light-2">{label}</p>
+    <p className="small-medium lg:base-medium text-dark-2">{label}</p>
   </div>
 );
 
